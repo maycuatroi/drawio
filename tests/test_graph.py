3087e7712fad888f6457f72a326d40aeed8e5514
Nguyễn Anh Bình <sometimesocrazy@gmail.com>
2023-01-03 21:34:12 +0700

    Add test case Add missing libs

---
+++ tests/test_graph.py
@@ -0,0 +1,19 @@
+from drawio import Graph, Node
+
+
+def test_graph():
+    node_a = Node("Christmas", shape="rectangle")
+    node_b = Node("Go shopping", shape="rectangle")
+    node_c = Node("Let me think", shape="oval")
+    node_d = Node("Laptop", shape="rectangle")
+    node_e = Node("iPhone", shape="rectangle")
+    node_f = Node("Car", shape="rectangle")
+
+    graph = Graph()
+    graph.add_nodes([node_a, node_b, node_c, node_d, node_e, node_f])
+    node_a >> "Get money" >> node_b
+    node_b >> node_c >> "One" >> node_d
+    node_c >> "Two" >> node_e
+    node_c >> "Three" >> node_f
+
+    csv_string = graph.to_draw_io_csv()
